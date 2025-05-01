ca34abb7fc01f19990611d031c77e69f49a8bc5d
brianguenter <1brianguenter@gmail.com>
2025-05-01 10:33:19 -0700

    removed import of OpticSimRepeatingStructures

--- src/OpticSimVisualization.jl
+++ src/OpticSimVisualization.jl
@@ -8,7 +8,7 @@ module OpticSimVisualization
 using OpticSim
 using OpticSim: euclideancontrolpoints, evalcsg, vertex, makiemesh, detector, centroid, lower, upper, intervals, α
 using OpticSim.Geometry
-import OpticSimRepeatingStructures as OpticSimRepeatingStructures
+
 
 using Unitful
 using ImageView
